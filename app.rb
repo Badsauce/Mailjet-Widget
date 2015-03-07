106b44d720bee4cb0ec82fec12ec06413e6c54b1
Badsauce <Ninjazoby@gmail.com>
2015-03-06 21:29:19 -0500

    exposed get access for debug

--- app.rb
+++ app.rb
@@ -33,4 +33,26 @@ class MyWebApp < Sinatra::Base
 
     {:status => 'ok', :message => 'Success, you have been added to our mailing list'}.to_json
   end
+
+  get '/addContact' do
+    content_type :json
+    #create new contact
+    @id = -1
+    begin
+      @contact = Mailjet::Contact.create(email: params['email'])
+      @id = @contact.id
+    rescue
+      return {:status => 'error', :message => 'An error occured when creating the contact'}.to_json
+    end
+
+    #add contact to mailing list
+
+    begin
+      Mailjet::Listrecipient.create(contact_id: @id, list_id: 2, is_active: true)
+    rescue
+      return {:status => 'error', :message => 'An error occured when adding the contact to the list'}.to_json
+    end
+
+    {:status => 'ok', :message => 'Success, you have been added to our mailing list'}.to_json
+  end
 end
