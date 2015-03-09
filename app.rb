b162d005c6a648ffb0a4de7e335b8ea8d8adcf54
Badsauce <Ninjazoby@gmail.com>
2015-03-09 17:23:14 -0400

    CORS configuration

--- app.rb
+++ app.rb
@@ -9,6 +9,12 @@ configure do
   enable :cross_origin
 end
 
+set :allow_origin, :any
+set :allow_methods, [:get, :post, :options]
+set :allow_credentials, true
+set :max_age, "1728000"
+set :expose_headers, ['Content-Type']
+
 class MyWebApp < Sinatra::Base
   get '/' do
 
