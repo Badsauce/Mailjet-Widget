5cc83b8c2775e0b5e93ff793c6eebbfe484071de
Badsauce <Ninjazoby@gmail.com>
2015-03-08 21:27:30 -0400

    removed insecure debug code

--- app.rb
+++ app.rb
@@ -15,30 +15,13 @@ class MyWebApp < Sinatra::Base
   post '/addContact' do
     content_type :json
     #create new contact
-    @id = -1
+    @id = nil
     begin
       @contact = Mailjet::Contact.create(email: params['email'])
       @id = @contact.id
     rescue
       return {:status => 'error', :message => 'An error occured when creating the contact'}.to_json
     end
-
-    #add contact to mailing list
-
-    begin
-      Mailjet::Listrecipient.create(contact_id: @id, list_id: 2, is_active: true)
-    rescue
-      return {:status => 'error', :message => 'An error occured when adding the contact to the list'}.to_json
-    end
-
-    {:status => 'ok', :message => 'Success, you have been added to our mailing list'}.to_json
-  end
-
-  get '/addContact' do
-    content_type :json
-    #create new contact
-    @contact = Mailjet::Contact.create(email: params['email'])
-    @id = @contact.id
 
     #add contact to mailing list
 
