370d9e5a4b20f4fa12604c0c74b9eb7e7e77a35e
Badsauce <Ninjazoby@gmail.com>
2015-03-09 23:54:22 -0400

    doing it live

--- app.rb
+++ app.rb
@@ -21,17 +21,17 @@ class MyWebApp < Sinatra::Base
       @contact = Mailjet::Contact.create(email: params['email'])
       @id = @contact.id
     rescue
-      return {:status => 'error', :message => 'An error occured when creating the contact'}.to_json
+      return {:status => 'error', :message => 'An error occured when creating the contact.'}.to_json
     end
 
     #add contact to mailing list
 
     begin
-      Mailjet::Listrecipient.create(contact_id: @id, list_id: 2, is_active: true)
+      Mailjet::Listrecipient.create(contact_id: @id, list_id: 1, is_active: true)
     rescue
-      return {:status => 'error', :message => 'An error occured when adding the contact to the list'}.to_json
+      return {:status => 'error', :message => 'An error occured when adding the contact to the list.'}.to_json
     end
 
-    {:status => 'ok', :message => 'Success, you have been added to our mailing list'}.to_json
+    {:status => 'ok', :message => 'Success, you have been added to our mailing list.'}.to_json
   end
 end
