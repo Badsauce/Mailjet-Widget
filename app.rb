611248d6b04fbe687a4f34c0a06f82b680e6bc85
Badsauce <Ninjazoby@gmail.com>
2015-03-11 23:56:50 -0400

    Subscribe no longer fails if a contact already exists

--- app.rb
+++ app.rb
@@ -15,37 +15,22 @@ class MyWebApp < Sinatra::Base
   post '/addContact' do
     content_type :json
     headers "Access-Control-Allow-Origin" => "*"
-    #create new contact
-    @id = nil
-    begin
-      @contact = Mailjet::Contact.create(email: params['email'])
-      @id = @contact.id
-    rescue
-      return {:status => 'error', :message => 'An error occured when creating the contact.', :code => 20 }.to_json
-    end
-
-    #add contact to mailing list
-
-    begin
-      Mailjet::Listrecipient.create(contact_id: @id, list_id: ENV['MJ_LIST_ID'].to_i, is_active: true)
-    rescue
-      return {:status => 'error', :message => 'An error occured when adding the contact to the list.', :code => 21}.to_json
-    end
-
-    {:status => 'ok', :message => 'Success, you have been added to our mailing list.', :code => 10}.to_json
-  end
-
-  get '/addContact' do
-    content_type :json
-    headers "Access-Control-Allow-Origin" => "*"
-    #create new contact
+    #locate or create the contact
     @id = nil
 
-      @contact = Mailjet::Contact.create(email: params['email'])
+      #attempt to find an existing contact
+      @contact = Mailjet::Contact.find(params['email'])
+
+      #create one if the search failed
+      unless @contact
+        begin
+          @contact = Mailjet::Contact.create(email: params['email'])
+        rescue
+          return {:status => 'error', :message => 'An error occured when creating the contact.', :code => 20 }.to_json
+        end
+      end
+
       @id = @contact.id
-
-      #return {:status => 'error', :message => 'An error occured when creating the contact.', :code => 20 }.to_json
-
 
     #add contact to mailing list
 
