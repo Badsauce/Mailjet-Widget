7c7ca85cdfb725a79941b05bb1311cc2acb5f400
Badsauce <Ninjazoby@gmail.com>
2015-03-11 23:01:38 -0400

    added list environment

--- app.rb
+++ app.rb
@@ -27,7 +27,30 @@ class MyWebApp < Sinatra::Base
     #add contact to mailing list
 
     begin
-      Mailjet::Listrecipient.create(contact_id: @id, list_id: 1, is_active: true)
+      Mailjet::Listrecipient.create(contact_id: @id, list_id: ENV['MJ_LIST_ID'].to_i, is_active: true)
+    rescue
+      return {:status => 'error', :message => 'An error occured when adding the contact to the list.', :code => 21}.to_json
+    end
+
+    {:status => 'ok', :message => 'Success, you have been added to our mailing list.', :code => 10}.to_json
+  end
+
+  get '/addContact' do
+    content_type :json
+    headers "Access-Control-Allow-Origin" => "*"
+    #create new contact
+    @id = nil
+
+      @contact = Mailjet::Contact.create(email: params['email'])
+      @id = @contact.id
+
+      #return {:status => 'error', :message => 'An error occured when creating the contact.', :code => 20 }.to_json
+
+
+    #add contact to mailing list
+
+    begin
+      Mailjet::Listrecipient.create(contact_id: @id, list_id: ENV['MJ_LIST_ID'].to_i, is_active: true)
     rescue
       return {:status => 'error', :message => 'An error occured when adding the contact to the list.', :code => 21}.to_json
     end
